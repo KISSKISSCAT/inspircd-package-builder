08468f00f7e2b4280b436771d04fe1b15b8a6d2d
ARKFUTURE <ARKFUTURE@FOXMAIL.COM>
2026-05-24 03:47:19 +0000

    update

--- deb/docker.sh
+++ deb/docker.sh
@@ -20,7 +20,9 @@ apt-get update
 apt-get install --assume-yes --no-install-recommends \
 	build-essential \
 	ca-certificates \
+	pkg-config \
 	libpsl-dev \
+	libtre-dev \
 	debhelper \
 	debhelper-compat \
 	wget \
